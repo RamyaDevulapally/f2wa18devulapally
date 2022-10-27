8158429c7fb9d40803074af40900b77cb30305bd
Ramya Devulapally <S547918@nwmissouri.edu>
2022-10-26 23:22:34 -0500

    Added the computation function

---
+++ routes/computation.js
@@ -0,0 +1,27 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+
+router.get('/', function(req, res, next) {
+  var num1= Math.abs(Math.random()*10)
+  var num2= Math.abs(Math.random()*10)
+  var num3= Math.abs(Math.random()*10)
+
+  var pow = Math.log2(num1)
+  var cosh = Math.cosh(num2)
+  var floor = Math.floor(num3)
+
+  res.render('computation', { title: 'Ramya Devulapally computation routes',
+  num1:num1,
+  num2:num2,
+  num3:num3,
+  log2:log2,
+  cosh:cosh,
+  floor:floor
+
+});
+
+});
+
+module.exports = router;
